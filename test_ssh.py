43307266250266ddf90880f6cf220904c8d35bf2
Kaban <piotrtokeny@gmail.com>
2026-01-04 17:09:08 +0100

    Usunieto pliki z danymi dostepu z repo

--- test_ssh.py
+++
@@ -1,7 +0,0 @@
-import paramiko
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('72.62.52.81', username='root', password='Ciapek515$$$')
-print('Polaczono!')
-ssh.close()
